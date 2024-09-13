ad27cfb37e8c08740b02f37dafe384472c5a11f1
Lucas Boucher <contact@lucasboucher.fr>
2024-09-13 13:36:05 +0200

    ADD: input

--- main.js
+++ main.js
@@ -3,13 +3,22 @@ import { initPWA } from "./pwa.js";
 import { translateToMorse } from "./utils/morse.js";
 
 const app = document.querySelector("#app");
-const textToTranslate = "Hello HETIC";
-const morseTranslation = translateToMorse(textToTranslate);
 
 app.innerHTML = `
   <h1>Traduction en Code Morse</h1>
-  <p>Texte: ${textToTranslate}</p>
-  <p>Code Morse: ${morseTranslation}</p>
+  <input type="text" id="textInput" placeholder="Entrez le texte à traduire" />
+  <button id="translateButton">Traduire</button>
+  <p id="morseOutput"></p>
 `;
+
+const textInput = document.querySelector("#textInput");
+const translateButton = document.querySelector("#translateButton");
+const morseOutput = document.querySelector("#morseOutput");
+
+translateButton.addEventListener("click", () => {
+  const textToTranslate = textInput.value;
+  const morseTranslation = translateToMorse(textToTranslate);
+  morseOutput.textContent = `Code Morse: ${morseTranslation}`;
+});
 
 initPWA(app);
